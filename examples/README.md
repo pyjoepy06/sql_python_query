fd95c2cfb87c5837006f17962a9e5ced8153071a
Joel Gray <32583151+pyjoepy06@users.noreply.github.com>
2021-06-03 15:20:28 -0400

    Examples Folder

---
+++ examples/README.md
@@ -0,0 +1,5 @@
+# Example of How to use the code for pymysql with AWS RDS Server
+
+I have created a RDS server in Amazon which has a database name: testDB and is using the following Tables
+
+![RDS_testDB_Tables](../docs/Database_Screenshot.jgp?raw=true "RDS testDB Database Tables")
--- examples/example_main.py
+++ examples/example_main.py
@@ -12,40 +12,55 @@ def db_connection(hostname,username,password,db_name,port_number):
     return(connection)
 
 
-def example_sql_query(hostname,username,password,db_name,port_number):
+def cars_example_sql_query(hostname,username,password,db_name,port_number):
     """This is a example function which will query * in a specific table"""
-    #Create a empty list of data
     sql_data = []
-    
-    #creating connection to the database
     connection = db_connection(hostname,username,password,db_name,port_number)
-    
-    #Setting the cursor
     cursor = connection.cursor()
-    
-    #Execute the SQL Query using execute() method, replace db_table_here with your db_table #
-    #Or SELECT specific columns 
-    #Or add your specific SQL query
-    #Or pass in variables of your own
-    
-    cursor.execute ("""SELECT * FROM db_table_here""")
-    
-    #fetching all records
-    
-    data = cursor.fetchall()
+    cursor.execute ("""SELECT * FROM Cars""")
+    sql_data = cursor.fetchall()
     return sorted(sql_data)
 
-def csv_report_creator(sql_data):
-    #Creates a csv file with all your data
+def persons_example_sql_query(hostname,username,password,db_name,port_number):
+    """This is a example function which will query * in a specific table"""
+    sql_data = []
+    connection = db_connection(hostname,username,password,db_name,port_number)
+    cursor = connection.cursor()
+    cursor.execute ("""SELECT * FROM Persons""")
+    sql_data = cursor.fetchall()
+    return sorted(sql_data)
 
-    with open('my_report.csv', 'w', newline='') as f_handle:
+def specific_query_persons_example_sql_query(hostname,username,password,db_name,port_number):
+    """This is a example function which will query * in a specific table"""
+    sql_data = []
+    connection = db_connection(hostname,username,password,db_name,port_number)
+    cursor = connection.cursor()
+    cursor.execute ("""SELECT PersonID, LastName, City FROM Persons WHERE PersonID=1""")
+    sql_data = cursor.fetchall()
+    return sorted(sql_data)
+
+
+def cars_csv_report_creator(sql_data):
+    with open('cars_report.csv', 'w', newline='') as f_handle:
         writer = csv.writer(f_handle)
-        #Add the header/column names
-        header = ['make', 'style', 'color', 'plate']
+        header = ['make', 'model', 'year', 'location', 'available']
         writer.writerow(header)
-        
-        #Iterate over data and write to the csv file
-        
+        for row in sql_data:
+            writer.writerow(row)
+
+def persons_csv_report_creator(sql_data):
+    with open('persons_report.csv', 'w', newline='') as f_handle:
+        writer = csv.writer(f_handle)
+        header = ['person_id', 'FirstName', 'LastName', 'Address', 'City']
+        writer.writerow(header)
+        for row in sql_data:
+            writer.writerow(row)
+
+def specific_query_persons_csv_report_creator(sql_data):
+    with open('persons_report.csv', 'w', newline='') as f_handle:
+        writer = csv.writer(f_handle)
+        header = ['Person_id', 'LastName', 'City']
+        writer.writerow(header)
         for row in sql_data:
             writer.writerow(row)
 
@@ -53,14 +68,15 @@ def main():
     #print('This is example script using a already created SQL Database\n\n')
     username = input('Please enter your username for your Database: ')
     password = getpass(prompt='Database Password: ')
-    db_name = input('Database DNS Name: ')
+    hostname = input('Please enter Database IP or  hostname: ')
+    db_name = input('Database Name (my example: testDB): ')
     port_number = input ('Database Port number (1521 or 3306 or custom number): ')
-    
+
     #convert port_number into integer
     port_number = int(port_number)
-    
+
     # Executing Example Jobs
     sql_query_job = example_sql_query(hostname,username,password,db_name,port_number)
-    
+
     #Creating CSV File
     csv_report_creator(sql_query_job)
